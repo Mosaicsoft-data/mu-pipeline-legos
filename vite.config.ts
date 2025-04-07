0a9490dbbf2c44ff72b268e421d13f336addf180
keyurishahdev <keyurishahdev@gmail.com>
2025-04-07 13:29:46 -0500

    adding jsx support

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-import { pluginAPIRoutes } from "vite-plugin-api-routes";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({

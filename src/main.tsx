f90e430546ccbbde89423b9a7f875776b737b198
keyurishahdev <keyurishahdev@gmail.com>
2025-04-07 13:37:21 -0500

    updating imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,6 @@
 // This file is kept for compatibility reasons but will be replaced by Next.js routing
+import React from "react";
+
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'

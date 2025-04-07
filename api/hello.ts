aa70e40be542dde07863c4e229acd1856c6380fb
keyurishahdev <keyurishahdev@gmail.com>
2025-04-07 12:53:33 -0500

    severless updates

--- api/hello.ts
+++ api/hello.ts
@@ -1,5 +1,3 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  res.status(200).json({ message: 'Hello from Vercel!' });
+export default function handler(req, res) {
+  res.status(200).json({ message: 'It works with ESM!' });
 }
